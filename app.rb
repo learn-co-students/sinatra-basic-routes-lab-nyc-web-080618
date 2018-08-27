755e26b57555628efbab2638dedf547ccef98cf0
madtab7 <madtab7@gmail.com>
2018-08-27 12:30:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Maddy"
+  end
+
+  get '/hometown' do
+    "My hometown is Redwood City, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is March Madness"
+  end
+
 end
